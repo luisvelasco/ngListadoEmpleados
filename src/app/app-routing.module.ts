25f9580f43205544fb3ebb6df87df9ce2c8b2d24
Luis Velasco <luis.velasco@lbvcentroamerica.com>
2023-03-23 07:48:22 -0600

    Crud con Child router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,23 @@ import { Routes,RouterModule } from '@angular/router';
 import { PersonasComponent } from './personas/personas.component';
 import { FormularioComponent } from './personas/formulario/formulario.component';
 
-const routes : Routes = [{path:'',component: PersonasComponent},
+// con router normal 
+/* const routes : Routes = [{path:'',component: PersonasComponent},
                         {path:'personas',component: PersonasComponent},
                         {path:'personas/agregar',component: FormularioComponent},
                         {path:'personas/:id',component: FormularioComponent},
 
+] */
+
+// con router children
+
+const routes: Routes = [{ path: '', component: PersonasComponent },
+{
+  path: 'personas', component: PersonasComponent, children: [
+    { path: 'agregar', component: FormularioComponent },
+    { path: ':id', component: FormularioComponent },
+  ]
+}
 ]
 
 @NgModule({
